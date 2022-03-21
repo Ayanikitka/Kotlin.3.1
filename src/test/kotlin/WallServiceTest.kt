74b11c730a3f6044aacbed0e3cda68a52f4cf10a
Nikita Osipov <office-nick@yandex.ru>
2022-03-21 11:31:52 +0500

    some changes

---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,39 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceAddTest {
+
+    @Test
+    fun add() {
+        WallService.clearPosts()
+        val post = Post(
+            id = -1,
+            date = 12343
+        )
+        WallService.add(post)
+        assertTrue(post.id != 0)
+    }
+    @Test
+    fun update_existingId() {
+        WallService.clearPosts()
+        WallService.add(Post())
+        WallService.add(Post())
+        val update = Post(id = 2)
+
+        val isUpdate = WallService.update(update)
+
+        assertTrue(isUpdate)
+    }
+    @Test
+    fun update_notExistingId() {
+        WallService.clearPosts()
+        WallService.add(Post())
+        WallService.add(Post())
+        val update = Post(id = -1)
+
+        val isNotUpdate = WallService.update(update)
+
+        assertTrue(isNotUpdate)
+    }
+}
